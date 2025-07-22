f66d116e4929bf6f30ae2bfe7ab93c75f3c622af
Gabriel <priv@gabrielnyberg.com>
2025-07-22 12:20:18 +0200

    wipe boilerplate ui

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -60,9 +60,9 @@ void MyFirstVstAudioProcessorEditor::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setColour (juce::Colours::white);
-    g.setFont (juce::FontOptions (15.0f));
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+//    g.setColour (juce::Colours::white);
+//    g.setFont (juce::FontOptions (15.0f));
+//    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void MyFirstVstAudioProcessorEditor::resized()
